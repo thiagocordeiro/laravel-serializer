6a92823e8968726eb3e015eec952ed732003c59d
Thiago Cordeiro <thiagoguetten@gmail.com>
2023-03-10 14:55:52 -0300

    cloning current changes on composer

--- tests/install.sh
+++ tests/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+COMMIT_HASH="$1"
 rm -rf blog
 
 composer create-project laravel/laravel blog
 php fix-files.php
 
-composer require thiagocordeiro/laravel-serializer:dev-main -d blog/
+composer require thiagocordeiro/laravel-serializer:dev-main#$COMMIT_HASH -d blog/
 
 rm blog/routes/api.php
 cp fixtures/api.php blog/routes/
